8188158e582e6fea2bac64f4215e5b6f50e08eb1
webdevtoday <coderwd@gmail.com>
2025-03-05 01:05:24 +0200

    Add debounce function

---
+++ src/utils/index.js
@@ -0,0 +1,7 @@
+export const debounce = (callback, delay) => {
+  let timeoutId;
+  return (...args) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback(...args), delay);
+  };
+};
